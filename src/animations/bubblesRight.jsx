eb1d504cf153de5c7f6c78bef66b551e453a544c
KhallilB <khallil.bailey@presidioidentity.com>
2022-10-25 12:49:55 -0700

    init

---
+++ src/animations/bubblesRight.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './bubblesRight.css';
+
+const BubblesRight = () => {
+    return (
+        <div className="container">
+            <div className="bubble x1">BUBBLE</div>
+            <div className="bubble x2">BUBBLE</div>
+            <div className="bubble x3">BUBBLE</div>
+            <div className="bubble x4">BUBBLE</div>
+            <div className="bubble x5">BUBBLE</div>
+            <div className="bubble x6">BUBBLE</div>
+            <div className="bubble x7">BUBBLE</div>
+            <div className="bubble x8">BUBBLE</div>
+            <div className="bubble x9">BUBBLE</div>
+            <div className="bubble x10">BUBBLE</div>
+            <div className="bubble x11">BUBBLE</div>
+            <div className="bubble x12">BUBBLE</div>
+            <div className="bubble x13">BUBBLE</div>
+            <div className="bubble x14">BUBBLE</div>
+            <div className="bubble x15">BUBBLE</div>
+            <div className="bubble x16">BUBBLE</div>
+            <div className="bubble x17">BUBBLE</div>
+            <div className="bubble x18">BUBBLE</div>
+            <div className="bubble x19">BUBBLE</div>
+            <div className="bubble x20">BUBBLE</div>
+        </div>
+    )
+}
+
+export default BubblesRight;
