42b678a9fc7a5dfddfbe9d44e8dfa34b34860f69
KhallilB <khallil.bailey@presidioidentity.com>
2022-10-31 16:07:14 -0700

    fix

--- src/animations/bubblesRight.jsx
+++ src/animations/bubblesRight.jsx
@@ -4,26 +4,26 @@ import './bubblesRight.css';
 const BubblesRight = () => {
     return (
         <div className="container">
-            <div className="bubble x1">BUBBLE</div>
-            <div className="bubble x2">BUBBLE</div>
-            <div className="bubble x3">BUBBLE</div>
-            <div className="bubble x4">BUBBLE</div>
-            <div className="bubble x5">BUBBLE</div>
-            <div className="bubble x6">BUBBLE</div>
-            <div className="bubble x7">BUBBLE</div>
-            <div className="bubble x8">BUBBLE</div>
-            <div className="bubble x9">BUBBLE</div>
-            <div className="bubble x10">BUBBLE</div>
-            <div className="bubble x11">BUBBLE</div>
-            <div className="bubble x12">BUBBLE</div>
-            <div className="bubble x13">BUBBLE</div>
-            <div className="bubble x14">BUBBLE</div>
-            <div className="bubble x15">BUBBLE</div>
-            <div className="bubble x16">BUBBLE</div>
-            <div className="bubble x17">BUBBLE</div>
-            <div className="bubble x18">BUBBLE</div>
-            <div className="bubble x19">BUBBLE</div>
-            <div className="bubble x20">BUBBLE</div>
+            <div className="bubble x1" id="1" onClick={() => document.getElementById('1').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x2" id="2" onClick={() => document.getElementById('2').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x3" id="3" onClick={() => document.getElementById('3').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x4" id="4" onClick={() => document.getElementById('4').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x5" id="5" onClick={() => document.getElementById('5').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x6" id="6" onClick={() => document.getElementById('6').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x7" id="7" onClick={() => document.getElementById('7').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x8" id="8" onClick={() => document.getElementById('8').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x9" id="9" onClick={() => document.getElementById('9').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x10" id="10" onClick={() => document.getElementById('10').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x11" id="11" onClick={() => document.getElementById('11').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x12" id="12" onClick={() => document.getElementById('12').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x13" id="13" onClick={() => document.getElementById('13').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x14" id="14" onClick={() => document.getElementById('14').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x15" id="15" onClick={() => document.getElementById('15').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x16" id="16" onClick={() => document.getElementById('16').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x17" id="17" onClick={() => document.getElementById('17').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x18" id="18" onClick={() => document.getElementById('18').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x19" id="19" onClick={() => document.getElementById('19').style.opacity = "0"}>Bubbles</div>
+            <div className="bubble x20" id="20" onClick={() => document.getElementById('20').style.opacity = "0"}>Bubbles</div>
         </div>
     )
 }
